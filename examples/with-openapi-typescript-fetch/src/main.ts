cbf9af7a2e2fb137f16a38c7ac1e94ed5f30aa50
Hussard <adrien.ricartnoblet@gmail.com>
2023-11-17 15:52:43 +0100

    Generate spotify.ts and make it

--- examples/with-openapi-typescript-fetch/src/main.ts
+++ examples/with-openapi-typescript-fetch/src/main.ts
@@ -1,1 +1,39 @@
-export const client = {};
+import { Fetcher } from "openapi-typescript-fetch";
+import { paths } from "./generated/spotify";
+
+async function main() {
+  const fetcher = Fetcher.for<paths>();
+  fetcher.configure({
+    baseUrl: "http://localhost:5678",
+  });
+
+  const findPlaylistsByUser = fetcher
+    .path("/users/{user_id}/playlists")
+    .method("get")
+    .create();
+
+  const getTracksByPlaylist = fetcher
+    .path("/playlists/{playlist_id}/tracks")
+    .method("get")
+    .create();
+
+  const { data: playlists } = await findPlaylistsByUser({
+    limit: 1,
+    user_id: "abc123",
+  });
+
+  if (!playlists?.items?.length) {
+    return;
+  }
+
+  console.log(`${playlists.items[0].name}`);
+
+  const { data: tracks } = await getTracksByPlaylist({
+    playlist_id: playlists.items[0].id!,
+  });
+
+  console.log(`${tracks?.total} tracks`);
+  console.log(tracks?.items?.map((t) => t.track?.name));
+}
+
+main();
