7519711e6e04e24dc38418e8bf78b7b4b9580ed6
Hussard <adrien.ricartnoblet@gmail.com>
2023-12-14 19:44:58 +0100

    Generate spotify.ts and write the app

--- examples/with-oazapfts/src/main.ts
+++ examples/with-oazapfts/src/main.ts
@@ -1,1 +1,47 @@
-export const client = {};
+import {
+  TrackObject,
+  getPlaylistsByPlaylistIdTracks,
+  getUsersByUserIdPlaylists,
+} from "./generated/spotify";
+// import * as SpotifyAPI from "./generated/spotify";
+
+async function main() {
+  const playlistsResponse = await getUsersByUserIdPlaylists("abc123", {
+    limit: 1,
+  });
+  if (playlistsResponse.status !== 200) {
+    return;
+  }
+
+  const playlists = playlistsResponse.data;
+  if (!playlists.items?.length) {
+    return;
+  }
+
+  console.log(`${playlists.items[0].name}`);
+
+  const tracksResponse = await getPlaylistsByPlaylistIdTracks(
+    playlists.items[0].id!
+  );
+  if (tracksResponse.status !== 200) {
+    return;
+  }
+
+  const tracks = tracksResponse.data;
+  console.log(`${tracks?.total} tracks`);
+  console.log(
+    tracks?.items?.map((t) => {
+      // oneOf is not generated correctly:
+      // https://github.com/oazapfts/oazapfts/pull/503#issuecomment-1828480622
+      const track = t.track as unknown as TrackObject;
+
+      return track.name;
+    })
+  );
+}
+
+console.log({
+  // SpotifyAPI,
+});
+
+main();
